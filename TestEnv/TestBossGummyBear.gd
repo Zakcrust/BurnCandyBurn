91490ce4e92b1972f3df0cf03cfaf29ec07e784e
zakcrust <zakcrusted@gmail.com>
2020-09-09 22:50:11 +0700

    add BOSS GUM Mechanics

--- TestEnv/TestBossGummyBear.gd
+++ TestEnv/TestBossGummyBear.gd
@@ -1,31 +1,58 @@
-extends KinematicBody2D
+extends Enemy
 
 
 
 var velocity : Vector2 = Vector2()
 var GRAVITY : float = 1000
-var speed : float = 300
+var speed : float = 100
 var dash : float = 450
 var player : KinematicBody2D
 
 var dash_target : Vector2 = Vector2()
 
+var attack_counter : int = 0
+var attacking : bool = false
+
+
+var shockwave_scene : PackedScene = load("res://Scene/Enemy/ShieldShockwave.tscn")
+var spear_scene : PackedScene = load("res://Scene/Enemy/Spear.tscn")
+
+
+signal add_shockwave(obj, obj_position)
+signal add_spear(obj, obj_position)
+
 enum {
+	IDLE
 	MOVE,
-	DASH
+	DASH,
+	SLAM,
+	ATTACK,
+	THROWSPEAR
 }
 
-var state = MOVE
+var state = IDLE
+
+func _init().(60, 0):
+	pass
+	
+
 
 func _process(delta):
+	print("GUM State : %s" % state)
 	velocity.x = 0
 	velocity.y += GRAVITY * delta
 	
 	match(state):
+		IDLE:
+			pass
 		MOVE:
-			_dash_check()
+			_check_player_position()
+			velocity.x = speed
+			$Body.play("move_spear")
 		DASH:
 			position = position.linear_interpolate(dash_target, delta)
+		ATTACK:
+			pass
 
 	velocity = move_and_slide(velocity, Vector2.UP)
 
@@ -37,11 +64,89 @@ func _dash_check():
 		state = DASH
 		$DashTimer.start()
 
+func _check_player_position() -> void:
+	if player.global_position.x < global_position.x:
+		if speed > 0:
+			speed = -speed
+			$Body.scale.x = 1
+	else:
+		speed = abs(speed)
+		$Body.scale.x = -1
+
 
 func _on_PlayerDetector_body_entered(body):
 	if body is Player:
 		player = body
+		_check_player_position()
+		state = THROWSPEAR
+		$Body.play("spear_throw")
+		$ThrowingTimer.start()
+		$PlayerDetector/CollisionShape2D.call_deferred("set_disabled", true)
 
 
 func _on_DashTimer_timeout():
+	state = MOVE
+
+
+func _on_Body_animation_finished():
+	if $Body.animation == "shield_slam":
+		$SlamDelay.start()
+
+func _on_MeleeRange_body_entered(body):
+	if body is Player and state == MOVE and not attacking:
+		attacking = true
+		state = ATTACK
+		$Body.play("attack_init")
+		$AttackPropagate.start()
+
+
+func _on_AttackPropagate_timeout():
+	attack_counter += 1
+	$Body.play("attack")
+	$AttackDelay.start()
+	_check_attack_count()
+
+
+func _check_attack_count() -> void:
+	if attack_counter >= 3:
+		attack_counter = 0
+		$ToSlam.start()
+
+
+func _on_AttackDelay_timeout():
+	attacking = false
+	state = MOVE
+
+
+func _on_ToSlam_timeout():
+	state = SLAM
+	attacking = true
+	$Body.play("shield_slam")
+	$ShockWaveTimer.start()
+
+func _on_SlamDelay_timeout():
+	state = MOVE
+	attacking = false
+
+
+func _on_ShockWaveTimer_timeout():
+	var left_shockwave = shockwave_scene.instance()
+	left_shockwave.to_left()
+	var right_shockwave = shockwave_scene.instance()
+	right_shockwave.to_right()
+	emit_signal("add_shockwave", left_shockwave, $LeftWave.global_position)
+	emit_signal("add_shockwave", right_shockwave, $RightWave.global_position)
+
+
+func _on_ThrowingTimer_timeout():
+	var new_spear = spear_scene.instance()
+	if $Body.scale.x == -1:
+		new_spear.to_right()
+	else:
+		new_spear.to_left()
+	emit_signal("add_spear", new_spear, $Body/SpearPosition.global_position)
+	$ThrowingDelay.start()
+
+
+func _on_ThrowingDelay_timeout():
 	state = MOVE
